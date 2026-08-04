8d1c781ed5d782704610d2b5b519ac262f969f2a
Joel Reyes <joelreyes3219@gmail.com>
2026-08-04 16:47:37 -0600

    Update db.js

--- db.js
+++ db.js
@@ -7,6 +7,7 @@ const mysql = require('mysql2/promise');
 const fs = require('fs');
 const path = require('path');
 
+
 function getCfg() {
   const CONN = process.env.DATABASE_URL || process.env.MYSQL_URL || process.env.MYSQL_PUBLIC_URL;
   if (CONN) {
